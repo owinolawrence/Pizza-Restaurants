46cb4d80d4cebac2d87d250b5f7da3238788a3f8
owinolawrence <owinolawrence21@gmail.com>
2019-10-12 16:54:00 +0300

    create size and crust selection

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//  size Selection
+  function Size(){
+    var Size=document.getElementById("size").value;
+     return parseInt (Size);
+  }
+//   crust Selection
+  function Crust(){
+    var Crust=document.getElementById("crust").value;
+    return parseInt (Crust);
+    
+  }
