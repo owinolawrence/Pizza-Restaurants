475763168814c9afb7c4bca868a3023677871a2e
owinolawrence <owinolawrence21@gmail.com>
2019-10-12 17:00:12 +0300

    add topping function with select samples

--- js/script.js
+++ js/script.js
@@ -9,3 +9,51 @@
     return parseInt (Crust);
     
   }
+//   Number of pizza 
+  function getNumber(){
+    var getNumber=document.getElementById("getnumber").value;
+    return parseInt (getNumber);
+  }
+  
+//   toppings selection
+  function getSpinach(){
+    var Spinach = 0;
+    var spinach = document.getElementById("spinach");
+       if (spinach.checked === true) {
+        Spinach = 200;
+        
+    }
+    return parseInt (Spinach);
+  }
+  
+  function getPepper(){
+    var Pepper = 0;
+    var pepper = document.getElementById("pepper");
+       if (pepper.checked === true) {
+        Pepper = 200;
+        
+    }
+    return parseInt (Pepper);
+  }
+  
+  function getBacon(){
+    var Bacon = 0;
+    var bacon = document.getElementById("bacon");
+       if (bacon.checked === true) {
+        Bacon = 200;
+        
+    }
+    return parseInt (Bacon);
+  }
+  
+  function getSausage(){
+    var Sausage = 0;
+    var sausage = document.getElementById("sausage");
+       if (sausage.checked === true) {
+        Sausage = 200;
+        
+    }
+    return parseInt (Sausage);
+  }
+
+  
